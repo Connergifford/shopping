33b8cac3e99dfcac062ba4d7c92b0e719496788a
Connergifford <111781584+Connergifford@users.noreply.github.com>
2022-09-15 11:29:40 -0400

    Add files via upload

---
+++ shopping.py
@@ -0,0 +1,12 @@
+msi_rtxa5000_price = 4199.35
+gigabyte_aero_price = 4295.54
+razer_blade15_price = 3696.99
+asus_zephyrus_m16_price = 1849.79
+
+print("the most expensive laptop is $4295.54")
+print("the least expensive laptop is $1849.79")
+print("the rounded price of the MSI RTX 5000 is $4199")
+print("the rounded price of the Gigabyte Aero is $4296")
+print("the rounded price of the Razer Blade 15 is $3697")
+print("the rounded price of the Asus Zephyrus is $1850")
+print("the average price of all computers is $3510.42")
